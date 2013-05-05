03ecda1506a8ad9af7eff4eb9006711b0ba62d3c
Chase Cummings <chaseecummings@gmail.com>
2013-05-05 13:18:47 -0700

    new file:   contact.php 	modified:   css/style.css 	modified:   index.html 	new file:   test.html 	deleted:    contact.html 	deleted:    projects.html 	deleted:    resume.html

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$self = $_POST['self'];
+
+$mailTo = 'chaseecummings@gmail.com';
+$subject = 'Message from Pi-Website visitor: ' . $name;
+
+$body = 'From: ' . $name . "\n";
+$body .= 'Email: ' . $email . "\n\n";
+$body .= $message;
+
+$header = "From: $email\r\n";
+$header .= "Reply-To: $email\r\n";
+
+$mail_status = mail($mailTo, $subject, $body, $header);
+if($self) {
+    $self_status = mail($email, $subject, $body, $header);
+}
+?>
