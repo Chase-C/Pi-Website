9fb411bf8df640897a418ba963eed631c55f067c
Chase Cummings <chaseecummings@gmail.com>
2013-05-05 23:29:20 -0700

    contact.php.old

--- contact.php
+++ contact.php
@@ -1,26 +1,6 @@
 <?php
-$f_name = $_POST['name'];
-$f_email = $_POST['email'];
-$f_message = $_POST['message'];
 
-//$f_self = $_POST['self'];
-
-$mailTo = 'chaseecummings@gmail.com';
-$subject = 'Message from Pi-Website visitor: ' . $f_name;
-
-//$body = 'From: ' . $f_name . "\n";
-//$body .= 'Email: ' . $f_email . "\n\n";
-//$body .= $f_message;
-$body = "Test\r\nMessage\r\nYo";
-$body = wordwrap($body, 70, "\r\n");
-
-$header = "From: $f_email\r\n";
-$header .= "Reply-To: $f_email\r\n";
-
-$mail_status = mail("chaseecummings@gmail.com", "Test Subject", "Test Message");
-
-if($mail_status) {
-} else { ?>
+if(!mail("chaseecummings@gmail.com", "Test Subject", "Test Message")) {
     <script language="javascript" type="text/javascript">
         alert('Message failed to send');
         window.location = '/';
